33ef7c6247762b7f7f9754af4ed73f371c2f7591
Shuvo Shakh <146642709+ShuvoShakh18@users.noreply.github.com>
2024-03-06 21:45:29 +0600

    added index.html

---
+++ index.js
@@ -0,0 +1,45 @@
+
+let blur = document.querySelector("#curser-blur");
+
+document.addEventListener("mousemove", function(dets){
+
+
+  blur.style.left = dets.x-200 + "px"
+  blur.style.top = dets.y - 200 + "px"
+
+})
+
+
+
+
+
+gsap.to("#nav",{
+    backgroundColor : "#000",
+  
+     height : "100px",
+    duration :1 ,
+   scrollTrigger:{
+    trigger : "#nav",
+    scroller:"body",
+    //markers :true,
+    start:"top -10%",
+    end : "top -11%",
+    scrub : 1
+   }
+})
+
+
+gsap.to("#main",{
+    backgroundColor : "#000",
+    scrollTrigger : {
+  trigger : "#main ",
+  scroller : "body",
+  start : "top -25%",
+  end : "top -70%",
+  scrub : 2,
+    }
+})
+
+
+
+
